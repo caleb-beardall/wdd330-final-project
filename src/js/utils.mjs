26b625dafde4b1f6eed1e0ec43b9bd82e81f63d0
caleb-beardall <calebbeardall@byui.edu>
2025-07-31 21:21:15 -0600

    Initial commit. I created the src, css, js, and public folders. I also create the index.html, footer.html, header.html, main.js, utils.mjs, and styles.css files.

---
+++ src/js/utils.mjs
@@ -0,0 +1,25 @@
+export function renderWithTemplate(template, parentElement, data, callback) {
+    parentElement.innerHTML = template;
+    // I'm not sure if I'm going to need the this 'if' statement or
+    // the data and callback parameters yet
+    if (callback) {
+        callback(data);
+    }
+}
+
+async function loadTemplate(path) {
+    const res = await fetch(path);
+    const template = await res.text();
+    return template;
+}
+
+export async function loadHeaderFooter() {
+    const headerTemplate = await loadTemplate("../partials/header.html");
+    const footerTemplate = await loadTemplate("../partials/footer.html");
+
+    const headerElement = document.querySelector("#main-header");
+    const footerElement = document.querySelector("#main-footer");
+
+    renderWithTemplate(headerTemplate, headerElement);
+    renderWithTemplate(footerTemplate, footerElement);
+}
