40ca864a2a86235345bc8cf137ec25098fd79b09
caleb-beardall <calebbeardall@byui.edu>
2025-08-03 18:34:23 -0600

    Updated the News API to GNews in ExternalServices.mjs

--- js/ExternalServices.mjs
+++ js/ExternalServices.mjs
@@ -1,4 +1,4 @@
-// API Key: 0d6d17fe8da545d18fcbec1e55b416a5
+// GNews Key: 48b4a45aec6008b3e2cf081e61499698
 
 export default class ExternalServices {
     constructor(company, topic) {
@@ -13,18 +13,18 @@ export default class ExternalServices {
 
             // Determine which API need to be accessed
             if (this.topic === "news") {
-                url = `https://newsapi.org/v2/everything?q=${this.company}&language=en&sortBy=publishedAt&pageSize=10&apiKey=0d6d17fe8da545d18fcbec1e55b416a5`;
+                url = `https://gnews.io/api/v4/search?q=${this.company}&lang=en&max=10&apikey=48b4a45aec6008b3e2cf081e61499698`;
             } else if (this.topic === "stocks") {
                 // In the future, fetch from a different API for stock data
-                url = `https://newsapi.org/v2/everything?q=${this.company}&language=en&sortBy=publishedAt&pageSize=10&apiKey=0d6d17fe8da545d18fcbec1e55b416a5`;
+                url = `https://gnews.io/api/v4/search?q=${this.company}&lang=en&max=10&apikey=48b4a45aec6008b3e2cf081e61499698`;
             }
 
             // Prepare and return the API's .json data as a JS object
             const response = await fetch(url);
             const data = await response.json();
             return data;
-        
-        // Catch errors if there's a problem accessing the data
+
+            // Catch errors if there's a problem accessing the data
         } catch (error) {
             console.error("Error fetching data:", error);
         }
--- js/main.mjs
+++ js/main.mjs
@@ -8,9 +8,13 @@ export function displayWelcomeMsg() {
     const visited = getLocalStorage("visited");
 
     if (!visited) {
-        welcomeElement.textContent = "Thank you for visiting!";
+        welcomeElement.innerHTML = `
+        <h2>Welcome to the Stock Portfolio Manager!<h2>
+        <p>Stay informed and in control of your investments. This app helps you stay up to date on your key portfolio stocks by providing the latest news articles and real-time data insights—all in one place.<p>`;
         setLocalStorage("visited", true);
     } else {
-        welcomeElement.textContent = "Welcome back!";
+        welcomeElement.innerHTML = `
+        <h2>Welcome back!<h2>
+        <p>Let's check in on your portfolio with the latest stock data and news updates.<p>`;
     }
 }
