f7b68dd47552cba8917329cfaff1511883a0411d
caleb-beardall <calebbeardall@byui.edu>
2025-08-04 22:37:46 -0600

    Started working on the apps CSS styling.

--- js/main.mjs
+++ js/main.mjs
@@ -9,12 +9,12 @@ export function displayWelcomeMsg() {
 
     if (!visited) {
         welcomeElement.innerHTML = `
-        <h2>Welcome to the Stock Portfolio Manager!<h2>
-        <p>Stay informed and in control of your investments. This app helps you stay up to date on your key portfolio stocks by providing the latest news articles and real-time data insights—all in one place.<p>`;
+        <h2>Welcome to the Stock Portfolio Manager!</h2>
+        <p>Stay informed and in control of your investments. This app helps you stay up to date on your key portfolio stocks by providing the latest news articles and real-time data insights—all in one place.</p>`;
         setLocalStorage("visited", true);
     } else {
         welcomeElement.innerHTML = `
-        <h2>Welcome back!<h2>
-        <p>Let's check in on your portfolio with the latest stock data and news updates.<p>`;
+        <h2>Welcome back!</h2>
+        <p>Let's check in on your portfolio with the latest stock data and news updates.</p>`;
     }
 }
